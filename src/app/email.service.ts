164772f2b90389012947e2d0b74f174067c5e6c2
Full Name <theofficialwebsiteguys@gmail.com>
2024-10-24 15:53:19 -0400

    form submission

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -10,7 +10,7 @@ export class EmailService {
   submitForm(formData: any) {
     const payload = {
       ...formData,
-      recipientEmail: 'theofficialwebsiteguys@gmail.com', // Pass the specific recipient email
+      businessEmail: 'theofficialwebsiteguys@gmail.com', // Pass the specific recipient email
     };
     return this.http.post(' https://twg-template-submission-92b1532f00c1.herokuapp.com/send-email-universal', payload);
   }
